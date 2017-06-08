0447c981ce343e83283605aa91ef4051f9e57156
Jay Pfaffman <jay@pfaffman.com>
2017-06-08 10:50:07 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Discourse Avatar name_first_letter
+
+Adds the ability to use `name_first_letter` in site setting `external system avatars url`.
+
+The default value for `external system avatars url` is `/letter_avatar_proxy/v2/letter/{first_letter}/{color}/{size}.png`. This plugin allows you to use `/letter_avatar_proxy/v2/letter/{name_first_letter}/{color}/{size}.png` to have the avatar be the first letter of the name rather than username. If there is no name, the username is used.
---
+++ plugin.rb
@@ -0,0 +1,32 @@
+# name: avatar-name-first-letter
+# about: Add name_first_letter as an option for external_system_avatars_url
+# version: 0.1
+# author: Jay Pfaffman
+# url: https://github.com/pfaffman/discourse-avatar-name-first-letter.git
+
+after_initialize do
+  require_dependency 'user'
+  class ::User
+
+    def self.system_avatar_template(username)
+      # TODO it may be worth caching this in a distributed cache, should be benched
+      if SiteSetting.external_system_avatars_enabled
+        url = SiteSetting.external_system_avatars_url.dup
+        url.gsub! "{color}", letter_avatar_color(username.downcase)
+        url.gsub! "{username}", username
+        url.gsub! "{name_first_letter}", get_name_first_letter(username)
+        url.gsub! "{first_letter}", username[0].downcase
+        url.gsub! "{hostname}", Discourse.current_hostname
+        url
+      else
+        "#{Discourse.base_uri}/letter_avatar/#{username.downcase}/{size}/#{LetterAvatar.version}.png"
+      end
+    end
+
+    def self.get_name_first_letter(username)
+      user = User.find_by_username(username)
+      user.name.presence ? user.name[0].downcase : username[0].downcase
+    end
+  end
+
+end
